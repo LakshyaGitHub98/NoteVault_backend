c98a721a7f2e919c016e4c07318c27b97423a328
LakshyaGitHub98 <laktripathi9839@gmail.com>
2025-10-01 16:37:51 +0530

    Fixing bugs 3.0

--- routes/authentication/userAuth.js
+++ routes/authentication/userAuth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../../controllers/auth/AuthController");
+const authController = require("../../controllers/auth/authController");
 const {
   checkForAuthentication,
   requireVerified,
