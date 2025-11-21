c6a4f599929dcfc6537a65592b93a8ce5c8b08e6
LakshyaGitHub98 <laktripathi9839@gmail.com>
2025-11-21 14:32:12 +0530

    1234

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -86,7 +86,7 @@ class AuthController {
       return res.status(200).json({ message: `OTP sent to ${email}` });
     } catch (err) {
       console.error("Send OTP error:", err);
-      return res.status(500).json({ error: "Failed to send OTP" });
+      return res.status(500).json({ error: err.message });
     }
   }
 
--- services/auth/mailer.js
+++ services/auth/mailer.js
@@ -3,13 +3,11 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: "127.0.0.1",
-  port: 587,
-  secure: false,
-  auth: {
-    user:process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+  service: 'gmail', 
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 
